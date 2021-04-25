14c957a4afb69eedd10e3b41ab091958f75f9bd3
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2021-04-25 14:50:23 -0300

    Projeto final

--- src/services/ConnectionsServices.ts
+++ src/services/ConnectionsServices.ts
@@ -13,11 +13,11 @@ interface IConnectionCreate {
 class ConnectionsService {
     private connectionsRepository: Repository<Connection>
 
-    constructor(){
+    constructor() {
         this.connectionsRepository = getCustomRepository(ConnectionsRepository);
     }
 
-    async create({ socket_id, user_id, admin_id, id }: IConnectionCreate){
+    async create({ socket_id, user_id, admin_id, id }: IConnectionCreate) {
         const connection = this.connectionsRepository.create({
             socket_id,
             user_id,
@@ -30,10 +30,41 @@ class ConnectionsService {
         return connection;
     }
 
-    async findByUserId(user_id: string){
+    async findByUserId(user_id: string) {
         const connection = await this.connectionsRepository.findOne({
             user_id,
-        })
+        });
+
+        return connection;
+    }
+
+    async findAllWithoutAdmin() {
+        const connections = await this.connectionsRepository.find({
+            where: {
+                admin_id: null
+            },
+            relations: ['user']
+        });
+
+        return connections;
+    }
+
+    async findBySocketId(socket_id: string) {
+        const connection = await this.connectionsRepository.findOne({
+            socket_id
+        });
+
+        return connection;
+    }
+
+    async updateAdminId(user_id: string, admin_id: string) {
+        await this.connectionsRepository
+        .createQueryBuilder().update(Connection)
+        .set({
+            admin_id
+        }).where('user_id = :user_id', {
+            user_id
+        }).execute();
     }
 }
 
